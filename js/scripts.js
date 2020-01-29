3b268da074938ae844656aa6ade8b1c6113b2e5d
Fatma Dogan and Kristina Hengster Tintor <kristina@>
2020-01-28 16:45:20 -0800

    add assignPrice prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
 function TicketStore () {
   this.tickets = [];
   this.currentId = 0;
+  this.currentPrice = 10
 }
 
-function Ticket(movie, time, age) {
+function Tickets(movie, time, age) {
   this.movie = movie;
   this.time = time;
   this.age = age;
+
 }
 
 TicketStore.prototype.addTicket = function(ticket){
@@ -19,31 +21,38 @@ TicketStore.prototype.assignId = function(){
   return this.currentId;
 }
 
-var ticketPrice = function(){
-  if (18<age<60){
-    if(time === "morning") {
-      return "$8"
-    } else if (time === "noon"){
-      return "$10"
-    } else if ( time === "night"){
-      return "$12"
+TicketStore.prototype.assignPrice = function() {
+   
+    if (18<age<60){
+      if(time === "morning") {
+        return this.curentPrice -= 2;
+      } else if (time === "noon"){
+        return this.curentPrice; 
+      } else if ( time === "night"){
+        return this.currentPrice +=2;
+      }
+    } else {
+      if( time === "morning"){
+        return this.curentPrice -= 3;
+      }else if( time === "noon"){
+        return this.currentPrice -= 1;
+      } else if ( time === "night") {
+        return this.currentPrice += 1;
+      }
     }
-  } else {
-    if( time === "morning"){
-      return "$7"
-    }else if( time === "noon"){
-      return "$9"
-    } else if ( time === "night") {
-      return "$11"
-    }
-  }
+    ticket.price = this.assignPrice();
 }
 
 // User Interface Logic
 var ticketStore = new TicketStore();
 
 function displayTicketDetails(ticketDetailsToDisplay){
-  var ticketsList = $("#ticket-info").append("your movie is " + tickets.movie + " at " + tickets.time + ". The price is " + ticketPrice);
+  var ticketsList = $("#ticket-info");
+  var htmlForTicketInfo = "";
+  ticketDetailsToDisplay.tickets.forEach(function(ticket){
+    htmlForTicketInfo + "Your movie is " + ticket.movie + " at " + ticket.time + ". The price is " + ticket.price
+  });
+  ticketsList.html(htmlForTicketInfo);
 }
 
 console.log(ticketStore);
@@ -52,15 +61,15 @@ $(document).ready(function() {
 $("form").submit(function(event){
   event.preventDefault();
 
-  var movie = $("select#movie").val();
-  var time = $("select#time").val();
+  var movie = $("#movie").val();
+  var time = $("#time").val();
   var age = parseInt($("input#age").val());
 
   $("select#movie").val("");
   $("select#time").val("");
   $("input#age").val("");
 
-  var newTicket = new Ticket(movie, time, age);
+  var newTicket = new Tickets(movie, time, age);
   ticketStore.addTicket(newTicket);
   displayTicketDetails(ticketStore);
 });
